31db5ab122b0fde002f96fcdf877a157dee96de4
skval55 <skvaladez@gmail.com>
2023-07-05 19:46:12 -0600

    finished part 2

--- react-1/addCommas.js
+++ react-1/addCommas.js
@@ -1,3 +1,22 @@
-function addCommas() {}
+function addCommas(num) {
+  let decNum = "";
+  let numberToIndex = 3;
+  let numWithCommas = num.toString().split("");
+  if (numWithCommas.indexOf(".") !== -1) {
+    decNum = numWithCommas.splice(
+      numWithCommas.indexOf("."),
+      numWithCommas.length
+    );
+  }
+  let numLength = numWithCommas.length;
+  if (numWithCommas[0] === "-") numberToIndex = 4;
+  while (numLength > numberToIndex) {
+    numLength -= 3;
+    numWithCommas.splice(numLength, 0, ",");
+  }
+  return decNum !== ""
+    ? numWithCommas.concat(decNum).join("")
+    : numWithCommas.join("");
+}
 
 module.exports = addCommas;
--- react-1/addCommas.test.js
+++ react-1/addCommas.test.js
@@ -4,4 +4,29 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("works with 1 digit number", () => {
+    expect(addCommas(6)).toEqual("6");
+  });
+  test("works with 3 digit number", () => {
+    expect(addCommas(123)).toEqual("123");
+  });
+  test("works with 4 digit number", () => {
+    expect(addCommas(1234)).toEqual("1,234");
+  });
+  test("works with 7 digit number", () => {
+    expect(addCommas(1000000)).toEqual("1,000,000");
+  });
+  test("works with 10 digit number", () => {
+    expect(addCommas(1234567890)).toEqual("1,234,567,890");
+  });
+  test("works with 4 digit negative number", () => {
+    expect(addCommas(-2345)).toEqual("-2,345");
+  });
+  test("works with 5 digit number with decimals", () => {
+    expect(addCommas(-45000.65)).toEqual("-45,000.65");
+  });
+  test("works with 7 digit number with decimals", () => {
+    expect(addCommas(-2345000.65)).toEqual("-2,345,000.65");
+  });
 });
